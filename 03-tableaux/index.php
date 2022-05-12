4c9f78836fbfbeb9bba997eb7a6420c014706267
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-05-12 10:32:36 +0200

    Fonction count merge et push sur tableau

--- 03-tableaux/index.php
+++ 03-tableaux/index.php
@@ -5,14 +5,20 @@ $tableau = array();
 $tableau = array('Lundi', 'Mardi', 45, 451.12, true, false);
 $tableau = ['ffgfd', true, 45.1, 476, [0, 1, 2, 3, 4]];
 echo '<pre>';
-var_dump($tableau);
-print_r($tableau);
+// var_dump($tableau);
+// print_r($tableau);
 $tableau[] = "Janvier";
 // print_r($tableau);
-print_r($tableau[5]);
+// print_r($tableau[5]);
+count($tableau);
+array_push($tableau, 'valeur1(a_p)', 'valeur2(a_p)', 'etc(a_p)');
+$tableau2 = ['Cabriolet', 5, 'couleur'=>'rouge'];
+$tableau = array_merge($tableau, $tableau2);
+print_r($tableau);
+
 echo '<hr>MOYENNE';
-$tabNotes = [12,5,16,7];
-$moy = array_sum($tabNotes)/count($tabNotes);
+$tabNotes = [12, 5, 16, 7];
+$moy = array_sum($tabNotes) / count($tabNotes);
 echo "<br>$moy";
 
 
