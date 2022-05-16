9a0052881e078282542e493dfe244729aa4d8b09
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-05-16 09:45:11 +0200

    Liens internes php cours

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-phpinfo();
+// phpinfo();
 ?>
+<ul>
+    <li><a href="./01-syntaxe/index.php">Syntaxe</a></li>
+    <li><a href="./02-variables/index.php">Variables</a></li>
+    <li><a href="./03-tableaux/index.php">Tableaux(array)</a>
+        <ul>
+            <li><a href="https://www.php.net/manual/fr/book.array.php">Doc php array</a></li>
+            <li><a href="./03-tableaux/php-json.php">Php JSON</a></li>
+        </ul>
+    <li><a href="./04-conditions/index.php">Conditions</a>
+</ul>
