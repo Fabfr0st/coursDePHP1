52e202d31337925f783a1afdfa592cd1cb7b348c
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-05-16 09:48:23 +0200

    Condition else

--- 04-conditions/index.php
+++ 04-conditions/index.php
@@ -1,6 +1,8 @@
 <?php
 
-$age = 22;
+$age = 2;
 if ($age >= 18) {
     echo "Vous êtes majeur à $age ans. Vous pouvez voter.";
+} else {
+    echo "Vous ne pouvez pas voter car vous êtes trop jeune, vous avec $age ans.";
 }
