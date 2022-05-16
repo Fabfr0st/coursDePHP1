645e8297c7426083a39506df87208951dcc38cf1
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-05-16 10:01:41 +0200

    Exo condition heure

--- 04-conditions/index.php
+++ 04-conditions/index.php
@@ -6,3 +6,23 @@ if ($age >= 18) {
 } else {
     echo "Vous ne pouvez pas voter car vous êtes trop jeune, vous avec $age ans.";
 }
+
+echo '<hr>';
+$heure = date('H');
+echo "Il est $heure H<br>";
+if ($heure < 12) {
+    echo "On est le matin.";
+} else {
+    echo "On est le soir.";
+}
+
+echo "<br>";
+$heure = 15;
+echo "Il est $heure H<br>";
+if ($heure < 12) {
+    echo "On est le matin.";
+} elseif ($heure < 18) {
+    echo "On est l'après-midi.";
+} else {
+    echo "On est le soir.";
+}
