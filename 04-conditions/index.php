9f596bbd1cd1917e7810c06d3271b05a7d838cd1
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-05-16 11:56:15 +0200

    Condition sur la valeur et le type

--- 04-conditions/index.php
+++ 04-conditions/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$age = 2;
+/* $age = 2;
 if ($age >= 18) {
     echo "Vous êtes majeur à $age ans. Vous pouvez voter.";
 } else {
@@ -25,4 +25,31 @@ if ($heure < 12) {
     echo "On est l'après-midi.";
 } else {
     echo "On est le soir.";
+} */
+
+$a = 20;
+$b = 5;
+$c1 = 10;
+$c2 = '10';
+
+if ($b != 3) {
+    echo "La variable b: $b est différente de 3.";
+}
+echo "<br>";
+if ($c1 == $c2) {
+    echo "La valeur de c1: $c1 est égale à c2: $c2.";
+}
+echo "<br>";
+if ($c1 === $c2) {
+    echo "La valeur et le type de c1: $c1 sont égaux à ceux de c2: $c2.<br>";
+} else {
+    echo "Le type de c1: $c1 est différent de celui de c2: $c2.";
+}
+echo "<br>";
+if ($c2 != $c1) {
+    echo "$c2 est différent de la valeur de c1: $c1";
+}
+echo "<br>";
+if ($c2 !== $c1) {
+    echo "c2: $c2 est différent de la valeur et de type de c1: $c1";
 }
