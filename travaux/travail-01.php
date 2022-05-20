a7a4207afe710546493e16f93e0cf900a9f6a146
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-05-20 09:53:33 +0200

    Exercice travail-01

--- travaux/travail-01.php
+++ travaux/travail-01.php
@@ -13,4 +13,18 @@ Travail-01 :
 | 35 | 95 |
 | 11 | 51.8 |
  */
-
+$tempsC = [25, 03, 35, 11];
+$tempsF = [];
+echo "<table>
+        <tr>
+            <td>°C</td>
+            <td>°F</td>
+        </tr>";
+foreach ($tempsC as $tempC) {
+    $tempF = round(($tempC * 1.8) + 32,PHP_ROUND_HALF_UP);
+    $tempsF[] = $tempF;
+}
+for ($i=0; $i < count($tempsC); $i++) { 
+    echo "<tr><td>".$tempsC[$i]."</td><td>".$tempsF[$i]."</td></tr>";
+}
+echo "</table>";
