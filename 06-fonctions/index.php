31185e596563ea460f58e1c103fd5a719dbec54a
Fabien Lastère <flast.ceppic.cdi22@gmail.com>
2022-06-07 13:51:19 +0200

    fonction dimension depuis diagonale

--- 06-fonctions/index.php
+++ 06-fonctions/index.php
@@ -15,3 +15,17 @@ function calculerSurface(float|int $long, float|int $larg): float
 }
 
 echo calculerSurface(5.2, 4);
+
+function diagToDim(float $diag): string
+{
+    $diag = abs($diag);
+    $ratio = 16 / 9;
+    $largeur = $ratio * sqrt($diag ** 2 / ($ratio ** 2 + 1));
+    $hauteur = sqrt(($diag ** 2) / ($ratio ** 2 + 1));
+    $largeur = round($largeur * 2.54, 2);
+    $hauteur = round($hauteur * 2.54, 2);
+    
+    return "<p>hauteur: $hauteur cm, largeur: $largeur cm</p>";
+}
+
+echo diagToDim(24);
